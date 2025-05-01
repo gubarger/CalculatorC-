c229bb27ead7a93b27f3c3acb5f91716c3a467e6
gubarger <kirillkum2001@gmail.com>
2025-05-01 16:10:27 +0300

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+
+#include "Header.h"
+
+using namespace std;
+
+int main() {
+	Arithmetic<double, double, string, double> number{ 0, 0 };
+	number.Inputing();
+}
